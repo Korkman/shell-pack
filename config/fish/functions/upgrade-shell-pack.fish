c7cfad3e42206722179bb6ae841a6ae0cd78698b
Korkman <github@pierre-beck.de>
2021-04-29 00:23:28 +0200

    Fix: upgrade-shell-pack now working

--- config/fish/functions/upgrade-shell-pack.fish
+++ config/fish/functions/upgrade-shell-pack.fish
@@ -1,7 +1,7 @@
 function upgrade-shell-pack -d \
 	"Download & install latest shell-pack"
 	
-	(curl -s -L "https://github.com/Korkman/shell-pack/raw/latest/get.sh" | sh) || return
+	curl -s -L "https://github.com/Korkman/shell-pack/raw/latest/get.sh" | sh || return
 	
 	reload
 end
