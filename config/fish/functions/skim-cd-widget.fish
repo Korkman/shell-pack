d16936ce95698b28a5914dee95547ef6009087d5
Korkman <github@pierre-beck.de>
2021-04-29 17:12:52 +0200

    Clean-up

--- config/fish/functions/skim-cd-widget.fish
+++ config/fish/functions/skim-cd-widget.fish
@@ -1,12 +1,17 @@
-function skim-cd-widget -d "Change directory"
+function skim-cd-widget -d "Change directory (recusrive search)"
+	# NOTE: the behavior is very different from the original skim-cd-widget-one
+	# - it does not substitute the last token of the command with the
+	#   arrival directory, nor does it search for it
+	# - the commandline stays untouched, so you can chdir without ctrl-c
+
 	if [ "$argv[1]" = "--dotfiles" ]
 		skim-dotfiles yes
 	else
 		skim-dotfiles no
 	end
-	set -l commandline (__skim_parse_commandline)
-	set -l dir $commandline[1]
-	set -l skim_query $commandline[2]
+
+	set -l dir '.'
+	set -l skim_query ''
 
 	set -q SKIM_ALT_C_COMMAND; or set -l SKIM_ALT_C_COMMAND "
 	command find -L \$dir -xdev -mindepth 1 \\( $SKIM_DOTFILES_FILTER \\) -prune \
@@ -18,10 +23,7 @@ function skim-cd-widget -d "Change directory"
 		eval "$SKIM_ALT_C_COMMAND | "(__skimcmd)' --header "'skim recursive cd. search for and select dir or esc to abort.'" -m --query "'$skim_query'"' | read -l result
 
 		if [ -n "$result" ]
-			cd $result
-
-			# Remove last token from commandline.
-			commandline -t ""
+			cd "$result"
 		end
 	end
 
