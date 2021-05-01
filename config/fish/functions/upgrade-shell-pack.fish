92588beeca39ebb4e3ad69940df446e6822b6aba
Korkman <github@pierre-beck.de>
2021-05-01 18:30:10 +0200

    upgrade-shell-pack: optional argument to download specific tag

--- config/fish/functions/upgrade-shell-pack.fish
+++ config/fish/functions/upgrade-shell-pack.fish
@@ -1,8 +1,13 @@
 function upgrade-shell-pack -d \
 	"Download & install latest shell-pack"
 	
+	set tag 'latest'
+	if ! set -q $argv[1]
+		set tag $argv[1]
+	end
+	
 	if [ "$UPGRADE_SHELLPACK" != "no" ]
-		curl -s -L "https://github.com/Korkman/shell-pack/raw/latest/get.sh" | sh || return 1
+		curl -s -L "https://github.com/Korkman/shell-pack/raw/$tag/get.sh" | sh -s || return 1
 		shell-pack-deps check
 		reload
 	else
