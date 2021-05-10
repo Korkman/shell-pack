01e3b1180a20b78727cb0d8bc4139b758d951ebc
Korkman <github@pierre-beck.de>
2021-05-10 21:41:10 +0200

    Add: instant deletion (f8) without extra steps Add: select multiple items for deletion Add: select multiple items for execution

--- config/fish/functions/__history_delete_commandline.fish
+++ config/fish/functions/__history_delete_commandline.fish
@@ -1,14 +1,27 @@
 function __history_delete_commandline \
 -d "Delete current commandline"
-	set -l deleted_history (commandline --current-buffer)
+	commandline --current-buffer | read -zl deleted_history    # capture newlines
+	set deleted_history (string escape -- $deleted_history) # somewhat easier to work on the escaped string
+	if test (string length -- $deleted_history) -gt 1 -a (string sub --start -2 --length 2 -- $deleted_history) = '\n'
+		# trailing newline detected, has to be removed for history to find
+		set deleted_history (string sub --end -2 -- $deleted_history)
+	end
+	#echo $deleted_history
+	#return
 	if test "$deleted_history" = ""
 		echo -ne "\a"
 		return
 	end
-	history delete --exact --case-sensitive "$deleted_history"
+	eval "history delete --exact --case-sensitive -- $deleted_history"
 	history save
-	echo
-	echo "Deleted from history: $deleted_history"
-	commandline --current-buffer --replace ""
-	commandline -f repaint
+	
+	# painting is terrible when clearing multiline buffer - borrowing the users keyboard
+	function __history_delete_commandline_info -e fish_prompt
+		functions -e __history_delete_commandline_info
+		__update_glyphs
+		echo (set_color red)"$deleted_glyph"(set_color normal)" History item deleted."
+	end
+	commandline --replace " " # a space triggers postexec
+	commandline -f execute
+	
 end
