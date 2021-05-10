7b2023a8847ca93effede73b95390784a231d7ba
Korkman <github@pierre-beck.de>
2021-05-10 21:48:13 +0200

    Add: instant deletion (f8) without extra steps Add: select multiple items for deletion Add: select multiple items for execution

--- config/fish/functions/skim-history-widget.fish
+++ config/fish/functions/skim-history-widget.fish
@@ -1,16 +1,45 @@
 function skim-history-widget -d "Show command history"
 	set -q SKIM_TMUX_HEIGHT; or set SKIM_TMUX_HEIGHT 40%
+	__update_glyphs
 	begin
 		set -lx SKIM_DEFAULT_OPTIONS "--height $SKIM_TMUX_HEIGHT $SKIM_DEFAULT_OPTIONS $SKIM_CTRL_R_OPTS -m"
-		set -lx bind (string escape -- 'f8:execute[echo -n " history delete --exact --case-sensitive "(string escape -- {})" && history save"]+abort')
+		set -lx bind (string escape -- 'f8:execute[printf \'\0delete\0\' ]+accept')
 		history -z \
-		| eval (__skimcmd) --read0 --print0 --no-multi --bind $bind --exact --tiebreak index \
+		| eval (__skimcmd) --read0 --print0  --bind $bind --exact --tiebreak index \
 		--header "'skim history. search for and select past cmd or esc to abort. f8 to delete.'" -q '(commandline)' \
-		| read -zl result
-		# NOTE: although nul delimited, read doesn't populate the list as intended
-		# so, no multi delete yet
-		#(count $result)
-		commandline -- $result
+		| while read -zl result; set -a results (string escape -- $result); end
+		
+		# magic prefix: an empty line (should be impossible in history), followed by delete
+		if test "$results[1]" = "''" -a "$results[2]" = "delete"
+			for result in $results[3..]
+				#echo "$result"
+				eval "history delete --exact --case-sensitive $result"
+			end
+			history save
+			echo (set_color red)"$deleted_glyph"(set_color normal)" Deleted "(math (count $results) - 2)" history item(s)"
+		else
+			if test (count $results) -gt 1
+				# Triple-space prefix to prevent any special handling and also to not
+				# submit combined content to history
+				commandline --replace (echo -e "   \n")
+				for result in $results[-1..1]
+					if test (string length -- $result) -gt 1 -a (string sub --start -2 --length 2 -- $result) = '\n'
+						# trailing newline detected, has to be removed here
+						#echo 1
+						#sleep 1
+						set result (string sub --end -2 -- $result)
+					end
+					#echo "$result"
+					
+					eval "commandline --append -- $result\\;\\n"
+				end
+				commandline --cursor 9999
+			else
+				eval "commandline -- $results[1]"
+			end
+			
+		end
+		
 	end
 	commandline -f repaint
 end
