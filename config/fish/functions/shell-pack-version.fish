e39d2304cdf6562eb36ad4f943eed4516a208f2d
Korkman <github@pierre-beck.de>
2021-05-12 15:09:27 +0200

    Comment

--- config/fish/functions/shell-pack-version.fish
+++ config/fish/functions/shell-pack-version.fish
@@ -4,5 +4,6 @@ function shell-pack-version \
 	# advantage: new shell-pack-releases that don't touch config.fish
 	# won't cause reloads anymore!
 	
+	# NOTE: the following line is parsed by shell-pack-check-upgrade!
 	echo '2.4'
 end
