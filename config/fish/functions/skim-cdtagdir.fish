36e58c99552bb2d5984ebe9a81d78d48a1b235d5
Korkman <github@pierre-beck.de>
2021-05-16 12:46:38 +0200

    List prepended with current $dirprev and $dirnext history

--- config/fish/functions/skim-cdtagdir.fish
+++ config/fish/functions/skim-cdtagdir.fish
@@ -1,10 +1,31 @@
-function skim-cdtagdir
+function skim-cdtagdir -d \
+	"cd into tagged and history directories"
 	set -l skim_cmd (__skimcmd)
 	
-	lsdirtags | sort | $skim_cmd --no-multi --reverse --height 40% --header 'enter:cd  ctrl-c:abort' --prompt "Fuzzy search in list: " | read -l result
+	begin
+		set -l mentioned "$PWD" # dedup list
+		for i in $dirnext
+			if contains -- "$i" $mentioned; continue; end
+			set -a mentioned "$i"
+			echo ">:$i"
+		end
+		for i in $dirprev[-1..1]
+			if contains -- "$i" $mentioned; continue; end
+			set -a mentioned "$i"
+			echo "<:$i"
+		end
+		lsdirtags | sort
+	end \
+	| $skim_cmd --ansi --no-multi --reverse --height 40% --header 'enter:cd  esc:abort' --prompt "cd tagged / history: " \
+	| read -l result
 	if [ -n "$result" ]
-		set result (string replace --regex ':.*' '' -- "$result")
-		cdtagdir "$result"
+		set dest (string replace --regex '.*?:' '' -- "$result")
+		# NOTE: while it is possible to navigate with prevd and nextd, it can be confusing when history is lost due to another cd
+		#if string match --quiet --regex '^>' -- "$result" && set idx (contains --index $dest $dirnext[-1..1])
+		#	nextd $idx
+		#else
+		cd "$dest"
+		#end
 	end
 	commandline -f repaint
 end
