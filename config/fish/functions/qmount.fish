e4258e8984ad33a9dadb5aa3d7a5b70759fdc910
Korkman <github@pierre-beck.de>
2021-05-19 00:00:58 +0200

    Better error handling qmount: allow specifying blockdevice with full path

--- config/fish/functions/qmount.fish
+++ config/fish/functions/qmount.fish
@@ -1,27 +1,42 @@
-#! /usr/bin/env -S fish -c qmount
+function qmount -d \
+	"Mount and browse devices with autocomplete"
+	# - creates directories derived from the device path in /run/q/
+	# - mounts with default options
+	# - cd && ls
+	
+	
+	if test "$argv[1]" = ""
+		echo "qmount will create /run/q/[device name], mount and cd there"
+		echo "arg #1: device name (without /dev) required"
+		return 1
+	end
+	
+	if test "$argv[2]" != ""
+		echo "no more than 1 argument allowed"
+		return 1
+	end
 
-# quick mount - mkdir and mount and cd and ls
-function qmount
-  if test "$argv[1]" = ""
-    echo "qmount will create /run/q/[device name], mount and cd there"
-    echo "arg #1: device name (without /dev) required"
-    return 1
-  end
-  
-  if test "$argv[2]" != ""
-    echo "no more than 1 argument allowed"
-    return 1
-  end
+	set -l devdisk "$argv[1]"
+	if test (string sub --start 1 --length 1 -- "$devdisk") != "/"
+		set devdisk "/dev/$devdisk"
+	end
 
-  set -l devdisk $argv[1]
-
-  if not blkid /dev/$devdisk
-    echo "/dev/$devdisk is not recognized by blkid"
-    return $status
-  end
-
-  mkdir -p /run/q/$devdisk
-  and mount /dev/$devdisk /run/q/$devdisk
-  and cd /run/q/$devdisk
-  and ls -al
+	blkid "$devdisk"
+	set -l rs $status
+	if not test $rs -eq 0
+		echo "$devdisk is not recognized by blkid"
+		return $rs
+	end
+	
+	set -l devshort (string replace --regex -- '^/(dev/)?' '' "$devdisk")
+	
+	if mountpoint -q "/run/q/$devshort"
+		echo "Target directory /run/q/$devshort is already occupied!"
+		return 3
+	end
+	
+	mkdir -p "/run/q/$devshort"
+	and mount "$devdisk" "/run/q/$devshort"
+	and cd "/run/q/$devshort"
+	and ls -al
 end
