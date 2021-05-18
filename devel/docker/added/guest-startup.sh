cfa22db2845d5ef58b74927c4b3f40928a238f48
Korkman <github@pierre-beck.de>
2021-05-18 19:27:05 +0200

    test-drive now adds unprivileged user for more testing

--- devel/docker/added/guest-startup.sh
+++ devel/docker/added/guest-startup.sh
@@ -1,30 +1,21 @@
 #! /bin/sh
+{
 
+# simulate pre-installed binaries
+if [ -e ~/Downloads/rg ]
+then
+	cp ~/Downloads/rg /usr/local/bin/rg
+fi
+if [ -e ~/Downloads/fzf ]
+then
+	cp ~/Downloads/fzf /usr/local/bin/fzf
+fi
+if [ -e ~/Downloads/sk ]
+then
+	cp ~/Downloads/sk /usr/local/bin/sk
+fi
 
-# autorun installer on first startup
-if [ "$AUTOSTART" = "yes" -a ! -e ~/.local/share/shell-pack/config ]; then
-	if [ -z "$LC_NERDLEVEL " ]; then
-		export LC_NERDLEVEL=3
-	fi
-
-	# simulate pre-installed binaries
-	if [ -e ~/Downloads/rg ]
-	then
-		cp ~/Downloads/rg /usr/local/bin/rg
-	fi
-	if [ -e ~/Downloads/fzf ]
-	then
-		cp ~/Downloads/fzf /usr/local/bin/fzf
-	fi
-	if [ -e ~/Downloads/sk ]
-	then
-		cp ~/Downloads/sk /usr/local/bin/sk
-	fi
-
-	FORCE_PRE_DOWNLOADED=y ~/Downloads/get.sh
-
-	FORCE_INSTALL_SP_PREFS=y bash -l
-
+onexit_copy_downloads() {
 	#echo "Checking if rg, sk were installed ..."
 	# copy back downloaded files for later use
 	if [ -e ~/.local/share/shell-pack/bin/rg ]
@@ -42,7 +33,33 @@ if [ "$AUTOSTART" = "yes" -a ! -e ~/.local/share/shell-pack/config ]; then
 		#echo "Caching sk .."
 		cp ~/.local/share/shell-pack/bin/sk ~/Downloads/sk
 	fi
+}
+
+trap onexit_copy_downloads exit term
+
+if [ -z "$LC_NERDLEVEL" ]; then
+	export LC_NERDLEVEL=3
+fi
+# setup an unprivileged user
+useradd shpuser
+mkdir -p /home/shpuser
+cp -aT /etc/skel /home/shpuser
+cp -a /root/Downloads /home/shpuser/
+chown -R shpuser:shpuser /home/shpuser
+echo "shpuser ALL=(ALL) NOPASSWD: ALL" > "/etc/sudoers.d/010_shpuser"
+chsh shpuser -s $(command -v fish)
+
+# autorun installer on first startup
+if [ "$AUTOSTART" = "yes" -a ! -e ~/.local/share/shell-pack/config ]; then
+	FORCE_PRE_DOWNLOADED=y ~/Downloads/get.sh
+	cd ~shpuser/Downloads
+	FORCE_PRE_DOWNLOADED=y su shpuser -c "./get.sh" > /dev/null
+	cd ~
+	FORCE_INSTALL_SP_PREFS=y bash -l
 else
-	export SHELL=$(which bash)
+	cd ~
 	bash -l
 fi
+
+exit
+}
