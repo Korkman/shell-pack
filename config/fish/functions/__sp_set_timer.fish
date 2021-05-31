ef5c7a293151c2e44a127606cde28ff1786c8373
Korkman <github@pierre-beck.de>
2021-05-31 19:34:07 +0200

    Made pulse subprocess more conservative: it will exit when fish is busy otherwise (mc, htop, ...) Pulse subprocess will cd before looping, closes #39

--- config/fish/functions/__sp_set_timer.fish
+++ config/fish/functions/__sp_set_timer.fish
@@ -17,25 +17,10 @@ function __sp_set_timer -a timer_name timer_seconds
 		set -ga __sp_timer_names "$timer_name"
 	end
 	
-	if ! set -q __sp_timer_pulse_pid && ! set -q __sp_timer_pulse_disable_kill
-		# start the pulse, sending a SIGUSR1 to this PID every $tick_interval in background
-		sh -c "trap 'noop=1' INT; while true; do sleep $tick_interval; kill -s USR1 $fish_pid || exit 0 > /dev/null 2>&1; done; exit 0" > /dev/null 2>&1 < /dev/null &
-		# store and disown the pid
-		set -g __sp_timer_pulse_pid (jobs --last --pid)
-		disown $__sp_timer_pulse_pid
-		# ready function to kill the PID on exit
-		function __sp_timer_kill_pulse --on-event fish_exit
-			if ! set -q __sp_timer_pulse_pid; return; end
-			#echo "Killing timer pulse PID $__sp_timer_pulse_pid"
-			
-			# find matching pid in ppid group, otherwise abort kill
-			if ps o pid,ppid | string match -q --regex "[^0-9]*""$__sp_timer_pulse_pid""[^0-9]+""$fish_pid"
-				kill $__sp_timer_pulse_pid
-			else
-				echo "Warning: Timer pulse bg process lost!"
-			end
-			set -ge __sp_timer_pulse_pid
-		end
+	if ! set -q __sp_timer_pulse_pid
+
+		__sp_timer_start_pulse
+
 	end
 end
 
@@ -44,12 +29,38 @@ function __sp_timer_check_pulse --on-event fish_postexec -d \
 	if set -q __sp_timer_pulse_pid
 		# find matching pid in ppid group, otherwise cancel pending timers && remove pid information
 		if ! ps o pid,ppid | string match -q --regex "[^0-9]*""$__sp_timer_pulse_pid""[^0-9]+""$fish_pid"
-			echo "Warning: Timer pulse bg process lost!"
-			echo "Pending timers cancelled: $__sp_timer_names"
-			set -ge __sp_timer_names
-			set -ge __sp_timer_pulse_pid
-			functions -e __sp_timer_kill_pulse
+			# restart pulse subprocess
+			#echo "debug: Timer pulse bg process restarting"
+			__sp_timer_start_pulse
 		end
+	end
+end
+
+function __sp_timer_start_pulse -d \
+	'Start pulse subprocess'
+	set -l tick_interval '0.5'
+	# start the pulse, sending a SIGUSR1 to this PID every $tick_interval in background
+	# cd / so the subprocess won't block mounts
+	sh -c "cd /; c=0; trap 'noop=1' INT; trap 'c=0' USR1; while [ \$c -lt 60 ]; do sleep $tick_interval; kill -s USR1 $fish_pid || exit 0; c=\$(( c + 1 )); done; exit 0" > /dev/null 2>&1 < /dev/null &
+	# store and disown the pid
+	set -g __sp_timer_pulse_pid (jobs --last --pid)
+	disown $__sp_timer_pulse_pid
+
+	set -g __sp_ticks_since_pulse_refresh 0
+
+	# ready function to kill the PID on exit
+	function __sp_timer_kill_pulse --on-event fish_exit
+		if ! set -q __sp_timer_pulse_pid; return; end
+		#echo "Killing timer pulse PID $__sp_timer_pulse_pid"
+		
+		# find matching pid in ppid group, otherwise abort kill
+		if ps o pid,ppid | string match -q --regex "[^0-9]*""$__sp_timer_pulse_pid""[^0-9]+""$fish_pid"
+			kill $__sp_timer_pulse_pid
+		else
+			# this is a normal thing now
+			#echo "Warning: Timer pulse bg process lost!"
+		end
+		set -ge __sp_timer_pulse_pid
 	end
 end
 
@@ -67,19 +78,32 @@ function __sp_timer_pulse -s SIGUSR1
 		end
 	end
 	
-	if set -q __sp_timer_pulse_disable_kill
-		# if this variable is set, rogue pulse mode is active (continue receiving SIGUSR1, work with it, do not kill PID)
-	else if ! set -q __sp_timer_pulse_pid
+	if ! set -q __sp_timer_pulse_pid
 		# if this variable does not exist, we received a SIGUSR1 without tracking the actual PID
-		#echo "Rogue pulse received (signal SIGUSR1 from unknown PID)"
-		set -g __sp_timer_pulse_disable_kill yes
+		# this may happen occasionally, when __sp_ticks_until_pulse_kill overlaps with received signal?
+		#echo "debug: Rogue pulse received (signal SIGUSR1 from untracked PID)"
 	else
 		# garbage collect pulse PID
 		set -g __sp_ticks_until_pulse_kill (math "$__sp_ticks_until_pulse_kill - 1")
 		if test "$__sp_timer_names" = "" && test "$__sp_ticks_until_pulse_kill" -le 0
 			__sp_timer_kill_pulse
 		end
+		# refresh pulse process
+		set -g __sp_ticks_since_pulse_refresh (math "$__sp_ticks_since_pulse_refresh + 1")
+		if test $__sp_ticks_since_pulse_refresh -gt 40
+			__sp_timer_refresh_pulse
+		end
 	end
 	
 end
 
+function __sp_timer_refresh_pulse -d \
+	'Send a SIGUSR1 to the pulse subprocess to refresh its self-termination countdown'
+	set -g __sp_ticks_since_pulse_refresh 0
+
+	#echo "debug: refreshing pulse"
+	if ps o pid,ppid | string match -q --regex "[^0-9]*""$__sp_timer_pulse_pid""[^0-9]+""$fish_pid"
+		#echo "debug: refreshing pulse!"
+		kill -s USR1 $__sp_timer_pulse_pid
+	end
+end
