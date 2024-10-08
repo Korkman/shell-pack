e178992740a50c699eb838027e5d66c64d900586
Korkman <github@pierre-beck.de>
2024-10-08 11:50:05 +0200

    More ssmart highlights

--- config/fish/functions/ssmart.fish
+++ config/fish/functions/ssmart.fish
@@ -16,5 +16,5 @@ function ssmart
 			set dev "/dev/$dev"
 		end
 	end
-	smartctl $argv[1..-2] -x "$dev" | less '+/.*(smartctl [0-9]|overall-health self-assessment|Reallocated_Sector_Ct|Wear_Leveling_Count|Uncorrectable_Error_Cnt|Seek_Error_Rate|Power_On_Hours|Current_Pending_Sector|Media_Wearout_Indicator|Reported_Uncorrect|Available Spare|Data Units Written|Power On Hours|Media and Data Integrity Errors).*$' +g
+	smartctl $argv[1..-2] -x "$dev" | less '+/.*(smartctl [0-9]|overall-health self-assessment|Reallocated_Sector_Ct|Wear_Leveling_Count|Uncorrectable_Error_Cnt|Seek_Error_Rate|Power_On_Hours|Current_Pending_Sector|Media_Wearout_Indicator|Reported_Uncorrect|Available Spare|Data Units Written|Power On Hours|Media and Data Integrity Errors|Test_Description|Raw_Read_Error_Rate).*$' +g
 end
