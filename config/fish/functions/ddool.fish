ae28447e5e48a1a7502bc57e67bc528f0d60f7fa
Korkman <github@pierre-beck.de>
2024-12-29 02:21:32 +0100

    ddool / ddstat: - fix: --clear functionality - remove: --tcp no longer in default args - add: --help / -h - add: --append / --remove for quick edits to saved args - add: interval-only arg handling
    Closes #75

--- config/fish/functions/ddool.fish
+++ config/fish/functions/ddool.fish
@@ -2,14 +2,94 @@
 
 # das dool with sane default parameters and auto-saved pass-thru params
 function ddool -w dool
+	# if not set, set default base parameters
+	if test -z "$ddool_base_args"
+		set -f ddool_base_args --bytes --time -cdrn
+	end
+	
+	if test "$argv[1]" = "--help"; or test "$argv[1]" = "-h"
+		# pass to dool
+		dool --help
+		# append help text
+		echo "----------------------------------------"
+		echo ""
+		echo "ddool persists arguments for dool, so next time you run ddool, it will"
+		echo "pass the same arguments."
+		echo ""
+		echo "Usage: ddool [dool arguments]"
+		echo "       ddool --append [dool arguments]"
+		echo "       ddool --remove [dool arguments]"
+		echo "       ddool --clear"
+		echo "       ddool INTERVAL"
+		echo ""
+		echo "--append / --remove can be used to add or remove select arguments"
+		echo ""
+		echo "--clear clears all saved arguments"
+		echo ""
+		echo "Just passing an interval will not save it"
+		echo ""
+		echo "Default arguments added are $ddool_base_args, which can be overridden:"
+		echo "set -U ddool_base_args $ddool_base_args"
+		echo "or reset:"
+		echo "set -eU ddool_base_args"
+		return
+	end
+	
+	# convert legacy variable $ddstat_addon_params to $ddool_addon_args
+	if test -z "$ddool_addon_args"
+		if test -n "$ddstat_addon_params"
+			set -U ddool_addon_args "$ddstat_addon_params"
+			set -eU ddstat_addon_params
+		end
+	end
+	
 	if test "$argv[1]" = "--clear"
-		set -U ddstat_addon_params ""
+		set -eU ddool_addon_args
+		set -eU ddstat_addon_params
+		echo "Cleared saved dool arguments. \$ddool_base_args:"
+		echo "$ddool_base_args"
+		return
 	end
-	if test "$argv" != "" -a "$argv[1]" != "--help"
-		set -U ddstat_addon_params "$argv"
+	
+	if test "$argv[1]" = "--append"
+		# shift argv to get remaining arguments, append them to $ddool_addon_args
+		set argv $argv[2..-1]
+		# iterate over arguments and append them
+		# only if string wasn't already appended
+		if ! string match -q -- "$argv" "$ddool_addon_args"
+			for arg in $argv
+				set -U -a ddool_addon_args "$arg"
+			end
+		end
+		# remove all arguments for remaining processing
+		set -e argv
 	end
-	if test "$ddstat_addon_params" != ""
-		echo "Starting ddool $ddstat_addon_params"
+	
+	if test "$argv[1]" = "--remove"
+		# shift argv to get remaining arguments, remove them from $ddool_addon_args
+		set argv $argv[2..-1]
+		set -U ddool_addon_args (string replace -r -- $argv $ddool_addon_args)
+		# remove all arguments for remaining processing
+		set -e argv
 	end
-	eval "dool -cdrn --tcp --time --bytes $ddstat_addon_params"
+	
+	# if only an interval is passed, do not persist it
+	set -l ddool_temp_args
+	if string match -q --regex -- '^([0-9]+)$' "$argv"
+		set -a ddool_temp_args "$argv"
+		set -e argv
+	end
+	
+	# persist arguments
+	if test "$argv" != ""
+		set -U ddool_addon_args $argv
+	end
+	
+	# print arguments as passed to dool
+	echo -n "dool"
+	for arg in $ddool_base_args $ddool_addon_args $ddool_temp_args
+		echo -n " "(string escape --style script -- $arg)
+	end
+	echo ""
+	dool $ddool_base_args $ddool_addon_args $ddool_temp_args
 end
--- config/fish/functions/ddstat.fish
+++ config/fish/functions/ddstat.fish
@@ -2,14 +2,85 @@
 
 # das dstat with sane default parameters and auto-saved pass-thru params
 function ddstat -w dstat
+	# if not set, set default base parameters
+	if test -z "$ddstat_base_args"
+		set -f ddstat_base_args --time -cdrn
+	end
+	
+	if test "$argv[1]" = "--help"; or test "$argv[1]" = "-h"
+		# pass to dstat
+		dstat --help
+		# append help text
+		echo "----------------------------------------"
+		echo ""
+		echo "ddstat persists arguments for dstat, so next time you run ddstat, it will"
+		echo "pass the same arguments."
+		echo ""
+		echo "Usage: ddstat [dstat arguments]"
+		echo "       ddstat --append [dstat arguments]"
+		echo "       ddstat --remove [dstat arguments]"
+		echo "       ddstat --clear"
+		echo "       ddstat INTERVAL"
+		echo ""
+		echo "--append / --remove can be used to add or remove select arguments"
+		echo ""
+		echo "--clear clears all saved arguments"
+		echo ""
+		echo "Just passing an interval will not save it"
+		echo ""
+		echo "Default arguments added are $ddstat_base_args, which can be overridden:"
+		echo "set -U ddstat_base_args $ddstat_base_args"
+		echo "or reset:"
+		echo "set -eU ddstat_base_args"
+		return
+	end
+	
 	if test "$argv[1]" = "--clear"
-		set -U ddstat_addon_params ""
+		set -eU ddstat_addon_params
+		echo "Cleared saved dstat arguments. \$ddstat_base_args:"
+		echo "$ddstat_base_args"
+		return
 	end
-	if test "$argv" != "" -a "$argv[1]" != "--help"
-		set -U ddstat_addon_params "$argv"
+	
+	if test "$argv[1]" = "--append"
+		# shift argv to get remaining arguments, append them to $ddstat_addon_params
+		set argv $argv[2..-1]
+		# iterate over arguments and append them
+		# only if string wasn't already appended
+		if ! string match -q -- "$argv" "$ddstat_addon_params"
+			for arg in $argv
+				set -U -a ddstat_addon_params "$arg"
+			end
+		end
+		# remove all arguments for remaining processing
+		set -e argv
 	end
-	if test "$ddstat_addon_params" != ""
-		echo "Starting ddstat $ddstat_addon_params"
+	
+	if test "$argv[1]" = "--remove"
+		# shift argv to get remaining arguments, remove them from $ddstat_addon_params
+		set argv $argv[2..-1]
+		set -U ddstat_addon_params (string replace -r -- $argv $ddstat_addon_params)
+		# remove all arguments for remaining processing
+		set -e argv
 	end
-	eval "dstat -cdrn --tcp --time $ddstat_addon_params"
+	
+	# if only an interval is passed, do not persist it
+	set -l ddstat_temp_args
+	if string match -q --regex -- '^([0-9]+)$' "$argv"
+		set -a ddstat_temp_args "$argv"
+		set -e argv
+	end
+	
+	# persist arguments
+	if test "$argv" != ""
+		set -U ddstat_addon_params $argv
+	end
+	
+	# print arguments as passed to dstat
+	echo -n "dstat"
+	for arg in $ddstat_base_args $ddstat_addon_params $ddstat_temp_args
+		echo -n " "(string escape --style script -- $arg)
+	end
+	echo ""
+	dstat $ddstat_base_args $ddstat_addon_params $ddstat_temp_args
 end
