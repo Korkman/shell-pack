114dbb078a7ec7e980939c6c2b1d331312582bd7
Korkman <github@pierre-beck.de>
2025-02-10 03:10:41 +0100

    be smarter about downloaded tar.* files

--- config/fish/functions/__sp_suggest_rename_file.fish
+++ config/fish/functions/__sp_suggest_rename_file.fish
@@ -9,10 +9,21 @@ function __sp_suggest_rename_file -d \
 	end >&2
 	
 	if ! type -q file
+		echo "$output_file"
+		return
+	end
+	if ! test -f "$output_file"
+		echo "File does not exist: $output_file" >&2
+		echo "$output_file"
 		return
 	end
 	
 	set mime_type (file -b --mime-type "$output_file")
+	if test "$status" -ne 0
+		echo "Execution of 'file' failed" >&2
+		echo "$output_file"
+		return
+	end
 	# --extension is supported from Debian Stretch and up, but often returns ??? for common mime types :-(
 	set valid_extensions (file -b --extension "$output_file" 2>/dev/null) || set valid_extensions '???'
 	if test "$valid_extensions" = "???"
@@ -27,11 +38,48 @@ function __sp_suggest_rename_file -d \
 				set valid_extensions "???"
 		end
 	end
-	# pick first extension as automatic
-	set auto_extension (string replace --regex '/.*' '' -- "$valid_extensions")
+	set valid_extensions_list (string split '/' -- "$valid_extensions")
+	# more hotfixes for file -b --extension
+	set -l keep_tar no
+	if contains -- "bz2" $valid_extensions_list
+		set -a valid_extensions_list "tb2" "tbz" "tbz2" "tz2"
+		set keep_tar yes
+	else if contains -- "gz" $valid_extensions_list
+		set -a valid_extensions_list "taz" "tgz"
+		set keep_tar yes
+	else if contains -- "lz" $valid_extensions_list
+		set keep_tar yes
+	else if contains -- "lzma" $valid_extensions_list
+		set -a valid_extensions_list "tlz"
+		set keep_tar yes
+	else if contains -- "lzo" $valid_extensions_list
+		set keep_tar yes
+	else if contains -- "xz" $valid_extensions_list
+		set -a valid_extensions_list "txz"
+		set keep_tar yes
+	else if contains -- "Z" $valid_extensions_list
+		set -a valid_extensions_list "tZ" "taZ"
+		set keep_tar yes
+	else if contains -- "zst" $valid_extensions_list
+		set -a valid_extensions_list "tzst"
+		set keep_tar yes
+	end
 	
-	# TODO: search current ending in list of valid endings returned by file
-	if test "$auto_extension" != "???" && ! string match -q --regex "\\.$auto_extension\$" -- "$output_file"
+	# pick first extension as automatic
+	set auto_extension "$valid_extensions_list[1]"
+	
+	# search current ending in list of valid endings returned by file
+	set output_file_ext (string replace --regex '.*\.' '' -- "$output_file")
+	if test -z "$output_file_ext"
+		set output_file_ext "???"
+	end
+	
+	if test "$auto_extension" != "???" && ! contains -- "$output_file_ext" $valid_extensions_list
+		# if output_file is a tar file, keep the tar extension
+		if test "$keep_tar" = "yes" && string match -qir '\.tar(\.|$)' -- "$output_file"
+			set auto_extension "tar.$auto_extension"
+		end
+		
 		set new_output_dir (dirname (realpath "$output_file"))
 		set new_output_file $new_output_dir"/"(string replace --regex '(\.tar){0,1}\.[^\.]+$' '' -- (basename "$output_file"))".$auto_extension"
 		
@@ -47,6 +95,7 @@ function __sp_suggest_rename_file -d \
 			if test (string lower -- "$answer") = "y" || test "$answer" = ""
 				mv "$output_file" "$new_output_file"
 				echo "$new_output_file"
+				return
 			end
 		end
 	end
