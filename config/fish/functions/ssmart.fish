a5864fcf13b1f3b831483e2845d1da57c4ffd930
Korkman <github@pierre-beck.de>
2025-12-16 03:34:06 +0100

    prepend "sudo" before smartctl if device is not readable (assuming it is readable for root) (closes #97)

--- config/fish/functions/ssmart.fish
+++ config/fish/functions/ssmart.fish
@@ -16,6 +16,13 @@ function ssmart
 			set dev "/dev/$dev"
 		end
 	end
+	
 	# pipe smartctl to less, highlighting key lines, jump to end then jump to start (working around bell when already at start)
-	smartctl $argv[1..-2] -x "$dev" | less '+/.*(smartctl [0-9]|overall-health self-assessment|Reallocated_Sector_Ct|Wear_Leveling_Count|Uncorrectable_Error_Cnt|Seek_Error_Rate|Power_On_Hours|Current_Pending_Sector|Media_Wearout_Indicator|Reported_Uncorrect|Available Spare|Data Units Written|Power On Hours|Media and Data Integrity Errors|Test_Description|Raw_Read_Error_Rate|% of test remaining).*$' '+G' '+g'
+	set -l cmd
+	if ! test -r "$dev"
+		set -a cmd "sudo"
+	end
+	set -a cmd smartctl $argv[1..-2] -x "$dev"
+	
+	$cmd | less '+/.*(smartctl [0-9]|overall-health self-assessment|Reallocated_Sector_Ct|Wear_Leveling_Count|Uncorrectable_Error_Cnt|Seek_Error_Rate|Power_On_Hours|Current_Pending_Sector|Media_Wearout_Indicator|Reported_Uncorrect|Available Spare|Data Units Written|Power On Hours|Media and Data Integrity Errors|Test_Description|Raw_Read_Error_Rate|% of test remaining).*$' '+G' '+g'
 end
