23a2ef27ff452c32d6b9592671f26e04fb0a4c6c
Korkman <github@pierre-beck.de>
2025-12-31 20:06:18 +0100

    fix: __sp_get_filesize for bsd

--- config/fish/functions/__sp_get_filesize.fish
+++ config/fish/functions/__sp_get_filesize.fish
@@ -1,5 +1,5 @@
 function __sp_get_filesize -a file -d \
 	'Get filesize in bytes'
-	set -l output (command ls -nl "$file" | string split ' ')
+	set -l output (command ls -nl "$file" | string split --no-empty ' ')
 	and echo "$output[5]"
 end
