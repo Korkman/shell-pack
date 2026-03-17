198bb9e0f3deec3473ca6a567d5f467e005c19a8
Korkman <github@pierre-beck.de>
2026-03-17 11:56:35 +0100

    fzf defaults: add ctrl-left/-right word jumps, colorize selected lines, --compact preset

--- config/fish/functions/__sp_fzf_defaults.fish
+++ config/fish/functions/__sp_fzf_defaults.fish
@@ -1,16 +1,23 @@
 function __sp_fzf_defaults -S -d \
 	"Set default options for fzf in \$fzf_defaults. Pass title as argument."
-	argparse e/exact -- $argv
+	argparse e/exact c/compact -- $argv
 	
 
 	set fzf_defaults --info inline-right --input-border=line --height=~80% --reverse \
 		"--color=dark hl:bright-yellow:reverse selected-hl:bright-yellow:reverse current-hl:bright-yellow:reverse header:#00ff87 header-label:#ffffff:bold" \
-		"--bind=esc:cancel"
+		"--color=selected-fg:#ffff00:bold selected-bg:#333300" \
+		"--bind=esc:cancel,ctrl-left:backward-word,ctrl-right:forward-word"
 
+	set -l ghost_add
+	if set -q _flag_compact
+		set ghost_add "$ghost_add, alt-b for more"
+		set -a fzf_defaults --bind "start:hide-header"
+	end
+	
 	if set -q _flag_exact
-		set -a fzf_defaults "--exact" "--ghost=[fzf query, exact match]"
+		set -a fzf_defaults "--exact" "--ghost=[fzf query, exact match"$ghost_add"]" --color "ghost:#6666cc"
 	else
-		set -a fzf_defaults "--ghost=[fzf query, fuzzy match]"
+		set -a fzf_defaults "--ghost=[fzf query, fuzzy match"$ghost_add"]" --color "ghost:#777700"
 	end
 	
 	if set -q argv[1]
