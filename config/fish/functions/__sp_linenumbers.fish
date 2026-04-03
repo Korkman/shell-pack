6827c7642e5b01db160b6f46961dd4655e4f8dfb
Korkman <github@pierre-beck.de>
2026-04-03 04:13:33 +0200

    more fixes to linenumbers

--- config/fish/functions/__sp_linenumbers.fish
+++ config/fish/functions/__sp_linenumbers.fish
@@ -2,26 +2,25 @@ function __sp_linenumbers -d \
 	"Add line numbers to input. Pass --total, --width and --undo to control behavior."
 	argparse u/undo w/width= t/total= -- $argv
 	
-	if ! set -q _flag_width
-		set _flag_width 6
+	if set -q _flag_undo
+		cat | string replace --regex "^.*?:" ""
+		return
 	end
 	
-	if test "$_flag_width" = "auto" && ! set -q _flag_undo
-		if ! set -q _flag_total
-			# count lines in tmp file to determine width
-			set -l tmpfile (mktemp __sp_linenumbers.XXXXXX)
-			cat > $tmpfile
-			wc -l < $tmpfile | string match -q --regex "^\s*(?<_flag_total>[0-9]+)"
-		end
-		set _flag_width (string length -- $_flag_total)
-		cat $tmpfile | __sp_linenumbers --width=$_flag_width
+	if ! set -q _flag_total && test "$_flag_width" = "auto"
+		# count lines in tmp file to determine width
+		set -l tmpfile (mktemp --tmpdir __sp_linenumbers.XXXXXX)
+		cat > $tmpfile
+		wc -l < $tmpfile | string match -q --regex "^\s*(?<_flag_total>[0-9]+)"
+		cat $tmpfile | __sp_linenumbers --width=auto --total=$_flag_total
 		rm $tmpfile
 		return
 	end
 	
-	if set -q _flag_undo
-		cat | string replace --regex "^.*?:" ""
-		return
+	if test "$_flag_width" = "auto"
+		set _flag_width (string length -- $_flag_total)
+	else if ! set -q _flag_width
+		set _flag_width 6
 	end
 	
 	# add line numbers with padding using awk
