f88a2fc9110a5d54d37452714611983ef1bd5197
Korkman <github@pierre-beck.de>
2026-07-18 01:28:54 +0200

    change: `man` alias now incorporates `onman` in flow when no local manpage exists

--- config/fish/functions/__sp_man_page.fish
+++ config/fish/functions/__sp_man_page.fish
@@ -11,11 +11,31 @@ function __sp_man_page
 		end
 	end
 	
+	set -l man_args_plain no
+	set -l man_arg_section
+	set -l man_arg_topic
+	if ! set -q argv[2] && ! string match -q --regex '^-' -- $argv[1]
+		# e.g. "man ls"
+		set man_args_plain yes
+		set man_arg_topic $argv[1]
+	else if ! set -q argv[3] && ! string match -q --regex '^-' -- $argv[1] && ! string match -q --regex '^-' -- $argv[2]
+		# e.g. "man 7 signal"
+		set man_args_plain yes
+		set man_arg_section $argv[1]
+		set man_arg_topic $argv[2]
+	end
+	
+	if test $man_args_plain = no
+		# e.g. "man --help" or "man -k keyword"
+		# doesn't translate to --help or onman calls
+		__sp_man_page_default $argv
+		return
+	end
+	
 	if command -q man
 		# test if fish's man preset finds the help
 		# (this introduces a slight latency, sorry. but it catches alternative 'man' use cases well.)
-		set man_output (PAGER=cat MANPAGER=cat __sp_man_page_default $argv 2>&1 | string collect --no-trim-newlines --allow-empty)
-		if test $pipestatus[1] = 0
+		if PAGER=cat MANPAGER=cat __sp_man_page_default $argv &> /dev/null
 			__sp_man_page_default $argv
 			return
 		else
@@ -26,23 +46,7 @@ function __sp_man_page
 		set man_status 99
 	end
 	
-	# exit early when complex args were passed as we have no way to translate them to --help or -h
-	if test (count $argv) -gt 1 || string match -q --regex '^-' -- $argv[1]
-		echo "$man_output" >&2
-		return $man_status
-	end
-	
-	# exit early when command does not exist
-	set -l search_cmd $argv[1]
-	if ! type -q -- $search_cmd
-		__sp_error "No man page found and not an executable or alias: $search_cmd"
-		return $man_status
-	end
-	
-	if test "$search_cmd" = "man"
-		__sp_error "No man page found and not an executable: $search_cmd"
-		return $man_status
-	end
+	set -l search_cmd $man_arg_topic
 	
 	# choose appropriate pager
 	if set -q MANPAGER
@@ -53,9 +57,7 @@ function __sp_man_page
 		set pager ppage
 	end
 	
-	# don't want to pass --help to just any command. working with whitelists here.
-	
-	# whitelist commands known to understand --help
+	# whitelist of commands to always pass --help to
 	# NOTE: theoretically a list of all commands known to support --help could go here
 	#       to support systems which lack man pages. attempting to strike some balance.
 	set wl_dash_dash_help \
@@ -63,10 +65,6 @@ function __sp_man_page
 		venv lsports lsnet cheat dl ssmart create qcrypt oldshell ddool nerdlevel \
 		cfc cfd qssh ggit qmount qumount ffingerprints ppage grasp mmux one rrg qchroot \
 		cclip \
-		# basics (portability: BSD utils don't support --help, but they will show usage - ignore the warning) \
-		tar cp mv chown chmod awk sed grep \
-		# tools
-		fzf rg dool wezterm scrcpy kitty code mysql mariadb wdotool \
 	;
 	
 	# allow dynamic whitelist to be added
@@ -85,70 +83,81 @@ function __sp_man_page
 
 	if contains -- $search_cmd $wl_dash_dash_help
 		begin
+			echo (set_color --bold brwhite)"NOTE:"(set_color normal)" No man page found, paging '$search_cmd --help' instead"(set_color normal)
+			echo ""
 			# close STDIN on search_cmd so any interactive input is cancelled
 			echo -n | $search_cmd --help
-			# hack: echo this to STDOUT so it appears at the end
-			echo "" >&1
-			echo (set_color brwhite)"NOTICE:"(set_color normal)" No man page found, paging '$search_cmd --help' instead"(set_color normal) >&1
 		end &| $pager
 		return
 	end
 
 	if contains -- $search_cmd $wl_dash_h
 		begin
+			echo (set_color --bold brwhite)"NOTE:"(set_color normal)" No man page found, paging '$search_cmd -h' instead"(set_color normal)
+			echo ""
 			# close STDIN on search_cmd so any interactive input is cancelled
 			echo -n | $search_cmd -h
-			# hack: echo this to STDOUT so it appears at the end
-			echo "" >&1
-			echo (set_color brwhite)"NOTICE:"(set_color normal)" No man page found, paging '$search_cmd -h' instead"(set_color normal) >&1
 		end &| $pager
 		return
 	end
+	# in case we were summoned in the commandline
+	echo >&2
 	__sp_error \
-		"No man page found and not whitelisted to support --help or -h: $search_cmd" \
+		"No man page found for: $search_cmd" \
 	;
+	echo >&2
 
 	# interactive fallback: offer to try --help or -h when stdin is a tty
 	if isatty stdin && isatty stdout
-		echo
-		echo "Try which flag?"
-		echo "  1) --help"
-		echo "  2) -h"
-		echo "  q) quit"
-		echo
-		read --prompt-str="Choice [1/2/q]: " --nchars 1 _sp_choice
-		echo
+		while true
+			echo "Alternatives to "(set_color $fish_color_command)"man "(set_color $fish_color_param)"$search_cmd"(set_color normal)
+			echo "  1|h) "(set_color $fish_color_command)"$search_cmd "(set_color $fish_color_param)"--help"(set_color normal)
+			echo "  2)   "(set_color $fish_color_command)"$search_cmd "(set_color $fish_color_param)"-h"(set_color normal)
+			echo "  3|o) "(set_color $fish_color_command)"onman "(set_color $fish_color_param)"$search_cmd   "(set_color $fish_color_comment)"# fetch man page from internet"(set_color normal)
+			echo "  4|c) "(set_color $fish_color_command)"cheat "(set_color $fish_color_param)"$search_cmd   "(set_color $fish_color_comment)"# fetch cheat sheet from cheat.sh"(set_color normal)
+			echo "  q) quit"
+			echo
+			read --prompt-str="Choice [1234hoc]: " --nchars 1 _sp_choice
+			echo
 
-		set -l chosen_var
-		set -l chosen_flag
-		
-		switch $_sp_choice
-		case 1
-			set chosen_flag --help
-			set chosen_var dash_dash_help_for_man
-		case 2
-			set chosen_flag -h
-			set chosen_var dash_h_for_man
-		case '*'
-			return $man_status
-		end
-
-		# temporarily whitelist and recurse
-		set -lx $chosen_var $$chosen_var
-		set -a $chosen_var $search_cmd
-		__sp_man_page $argv
-		
-		# offer to persist
-		echo
-		read --prompt-str="Persist '$search_cmd' in \$$chosen_var? [y/N]: " --nchars 1 _sp_persist
-		echo
-		if string match -qi y $_sp_persist
-			set -U -a $chosen_var $search_cmd
-			echo (set_color green)"Added '$search_cmd' to \$$chosen_var"(set_color normal)
+			set -l chosen_var
+			set -l chosen_flag
+			
+			switch $_sp_choice
+				case 1 h
+					set chosen_flag --help
+					set chosen_var dash_dash_help_for_man
+				case 2
+					set chosen_flag -h
+					set chosen_var dash_h_for_man
+				case 3 o 
+					onman $argv
+					continue
+				case 4 c
+					cheat $argv
+					continue
+				case '*'
+					return 0
+			end
+			
+			# temporarily whitelist and recurse
+			set -lx $chosen_var $$chosen_var
+			set -a $chosen_var $search_cmd
+			echo -n | __sp_man_page $argv
+			
+			# offer to persist
+			# TODO: is this a good thing? choice is easy to select, whitelist hard to undo ...
+			#echo
+			#read --prompt-str="Persist '$search_cmd' in \$$chosen_var? [y/N]: " --nchars 1 _sp_persist
+			#echo
+			#if string match -qi y $_sp_persist
+			#	set -U -a $chosen_var $search_cmd
+			#	echo (set_color green)"Added '$search_cmd' to \$$chosen_var"(set_color normal)
+			#end
 		end
 		return
 	end
 	
-	# no whitelist matched, return original man status
-	return $man_status
+	# fool fish_man_page: there's always a way
+	return 0
 end
