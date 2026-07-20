f2581d5ab2897f29f6211aef7803be8f4e69f82a
Korkman <github@pierre-beck.de>
2026-07-20 22:21:02 +0200

    new internal: __sp_osc8_url

---
+++ config/fish/functions/__sp_osc8_url.fish
@@ -0,0 +1,12 @@
+function __sp_osc8_url -a url -a text -d \
+	'OSC8 encode URL (and visible text) to show native link in supporting terminals'
+	printf "\e]8;;"
+	echo -n $url
+	printf "\e\\"
+	if test -n "$text"
+		echo -n $text
+	else
+		echo -n $url
+	end
+	printf "\e]8;;\e\\"
+end
