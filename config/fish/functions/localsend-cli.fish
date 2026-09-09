4ee9960216c9164aed5dd9b370a6d02faa81e75e
Korkman <github@pierre-beck.de>
2026-09-09 21:31:08 +0200

    fix: hide localsend hotkeys when version is queried

--- config/fish/functions/localsend-cli.fish
+++ config/fish/functions/localsend-cli.fish
@@ -1,7 +1,7 @@
 function localsend-cli -d "LocalSend CLI wrapper with on-demand installation"
 	# parse $argv but restore it afterwards
 	set -l argv_copy $argv
-	argparse -i 'h/help' -- $argv
+	argparse -i 'h/help' 'V/version' -- $argv
 	set argv $argv_copy
 	
 	if not command -q localsend-cli
@@ -11,7 +11,7 @@ function localsend-cli -d "LocalSend CLI wrapper with on-demand installation"
 
 	if command -q localsend-cli
 		# if help isn't requested anyways, print out the hotkeys as a reminder
-		if ! set -q _flag_help
+		if ! set -q _flag_help && ! set -q _flag_version
 			command localsend-cli --help | awk '/Environment Variables/{exit} /Hotkeys/{flag=1} flag'
 		end
 		command localsend-cli $argv
